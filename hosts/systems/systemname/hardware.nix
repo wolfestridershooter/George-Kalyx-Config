be64478a1a1868f50c1a17ad8667d7cd033c9e9e
Kyle Kubis <44762123+kyleraykbs@users.noreply.github.com>
2024-03-11 11:54:44 -0700

    Added pipewire to hardware.nix

--- hosts/systems/systemname/hardware.nix
+++ hosts/systems/systemname/hardware.nix
@@ -15,6 +15,12 @@
     intel.enable = lib.mkDefault false;
     amd.enable = lib.mkDefault false;
 
+    # Sound Options
+    sound = {
+      enable = true;
+      soundServer = "pipewire"; # This can be 'pipewire' (default) or 'pulse'.
+    };                          # NOTE: Pipewire can be enabled seperetly without audio using 'kalyx.pipewire.enable = true';
+
     # Networking Options
     broadcom.enable = lib.mkDefault true;
   };
