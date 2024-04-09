9715de997484387602f766889d1867974578f29d
kyleraykbs <kyleraykbs@proton.me>
2024-04-09 15:01:11 -0700

    Removed accidental inclusion of device hardware configuration

--- hosts/systems/system1/hardware.nix
+++ hosts/systems/system1/hardware.nix
@@ -5,12 +5,12 @@
 
   kalyx = {
     # GPU Options
-    intelgpu.enable = lib.mkDefault true;
+    intelgpu.enable = lib.mkDefault false;
     amdgpu.enable = lib.mkDefault false;
     nvidia.enable = lib.mkDefault false;
 
     # CPU Options
-    intel.enable = lib.mkDefault true;
+    intel.enable = lib.mkDefault false;
     amd.enable = lib.mkDefault false;
 
     # Sound Options
@@ -48,33 +48,5 @@
                                                                # Delete this if your existing config you are about to paste already has this option set as it may cause an error.
 
   # V Drop options of hardware-configuration.nix here V:
-boot.initrd.availableKernelModules = [ "xhci_pci" "ehci_pci" "ahci" "usb_storage" "sd_mod" "rtsx_pci_sdmmc" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/0aa34214-c84e-483f-a562-982bd6a6538f";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/ADFB-A8D5";
-      fsType = "vfat";
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/0eee51f9-0aa0-4a19-a320-bbc6c930bb18"; }
-    ];
-
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp0s25.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp3s0.useDHCP = lib.mkDefault true;
-
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
